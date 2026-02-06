11f09aa2adba66fb9e06f0f80619f4d7f133f63f
syastrebov <serg.yastrebov@gmail.com>
2026-02-06 17:32:27 +0300

    Provider enum

---
+++ src/CircuitBreaker/Enums/Provider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace CircuitBreaker\Enums;
+
+enum Provider: string
+{
+    case Redis = 'redis';
+    case Predis = 'predis';
+    case Memcached = 'memcached';
+    case Database = 'database';
+    case Memory = 'memory';
+}
